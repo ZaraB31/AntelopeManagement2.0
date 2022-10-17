8c4402430324ad78f36762604cf3f6a86d749af4
Zara Bostock <bostock.zara@gmail.com>
2022-10-17 13:49:49 +0100

    Laravel mix and sass setup, login page updated and styled

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="/dist/app.css">
+    <link rel="stylesheet" href="/dist/auth.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&family=Share:wght@400;700&display=swap" rel="stylesheet">
-    <title>Antelope Management</title>
+    <title>@yield('title')</title>
 </head>
 <body>
     <main>
