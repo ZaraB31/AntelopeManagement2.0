7adb8d6a327d0731f759f258174abb611932ce90
Zara Bostock <bostock.zara@gmail.com>
2022-10-17 14:57:21 +0100

    Models, Migrations and Controllers added

---
+++ database/migrations/2022_10_17_130427_create_projects_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->dateTime('deadline');
+            $table->string('link');
+            $table->boolean('completed');
+            $table->foreignId('company_id')->references('id')->on('companies')->onDelete('cascade');
+            $table->foreignId('employer_id')->references('id')->on('employers')->onDelete('cascade');
+            $table->foreignId('projectType_id')->references('id')->on('project_types')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
